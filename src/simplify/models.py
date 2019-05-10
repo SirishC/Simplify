22537f65cdadc89417c2e90d14995ed72ad0a1ba
Sirish C <sirishc@Sirishs-MacBook-Air.local>
2019-05-10 23:54:47 +0530

    code generation updation

--- src/simplify/models.py
+++ src/simplify/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 from .utils import code_generator , create_shortcode
 # Create your models here.
-
+class simpleManager(models.Manager):
+    def all(self,*args ,**kwargs):
+        qs = super(simpleManager,self).all(*args,**kwargs)
+        qs = qs.filter(active=True)
+        return qs
+    def refresh_code(self):
+        qs = simpleURL.objects.filter(id__gte=1)
+        code_count = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            q.save()
+            code_count +=1
+        return "code count is {i}".format(i=code_count)
 class simpleURL(models.Model):
     url         = models.CharField(max_length=220, )
-    shortcode   = models.CharField(max_length=15,unique=True,blank=True)
+    shortcode   = models.CharField(max_length=15,default='abc',unique=True,blank=True)
+    active      = models.BooleanField(default=True)
+    objects = simpleManager()
     def save(self,*args,**kwargs):
         self.shortcode = code_generator()
         print("code generated successfully !" )
--- src/simplify/utils.py
+++ src/simplify/utils.py
@@ -9,7 +9,7 @@ def code_generator(size=6,chars=string.ascii_lowercase+ string.ascii_uppercase +
 def create_shortcode(instance,size= 6):
     newshortcode = code_generator(size)
     Klass_simpleURL = instance.__class__
-    qs_exists  = Klass_simpleURL.object.filter(shortcode=newshortcode).exists()
+    qs_exists  = Klass_simpleURL.objects.filter(shortcode=newshortcode).exists()
     if qs_exists :
         return create_shortcode(size=6)
     return newshortcode
